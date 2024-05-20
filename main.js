c9f73bcf8b9365cf7d2c3a586e3cc5d1f89e660f
Matt-ya-Nata <mathewsmaps@gmail.com>
2024-05-20 04:42:21 +0200

    Ads Edits

--- main.js
+++ main.js
@@ -4,3 +4,31 @@ document.querySelector('#search-icon').onclick =() => {
     search.classList.toggle('active'); 
 }
 
+
+
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow',window.scrollY >0);
+});
+var formdata = new FormData();
+formdata.append("type", "Thick Horizontal");
+formdata.append("tags", "coffee");
+formdata.append("tags", "Cappuccino,Espresso");
+
+var ajax = new XMLHttpRequest();
+ajax.addEventListener("load", completeHandler, false);
+
+ajax.open("POST", "https://ad.simaneka.com/api/get");
+ajax.setRequestHeader("authorisation", "unk63DzzpkMVQX8FTdvpN0KbDYavAzWI");
+
+ajax.send(formdata);
+
+function completeHandler(event) {
+    var response = JSON.parse(event.target.responseText);
+
+    console.log(response);
+    document.querySelector('.advertIMG').src = response.link;
+    document.querySelector('.advertIMG').alt = response.alt;
+    document.querySelector('.anchorElement').href = response.href;
+    document.querySelector('.headerText').innerHTML = response.message;
+}
